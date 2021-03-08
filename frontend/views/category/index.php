fd601fc68c6528765aa2e3029ce5af3b2c915bb3
Vadim Prokoshin <vpro85@gmail.com>
2021-03-08 17:23:24 +0200

    Add display of "featured items" on category/index

--- frontend/views/category/index.php
+++ frontend/views/category/index.php
@@ -1,5 +1,6 @@
 <?php
 use common\widgets\MenuWidget;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
@@ -13,33 +14,25 @@ $this->title = 'My Yii Shop Application';
         </ul>
     </div>
     <div class="col-sm-9">
-        <div class="features_items">
-            <h2 class="text-center">Featured Products</h2>
-            <div class="col-sm-4">
-                <div class="product-image-wrapper">
-                    <div class="single-products">
-                        <div class="productinfo text-center">
-                            <img src="/images/products/product1.jpg" alt="Джинсы">
-                            <h2>$10</h2>
-                            <p><a href="/product/1">Джинсы</a></p>
-                            <a href="#" data-id="1" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+        <?php if(!empty($hits)): ?>
+            <div class="features_items">
+                <h2 class="text-center">Featured Products</h2>
+            <?php foreach ($hits as $hit): ?>
+                <div class="col-sm-4">
+                    <div class="product-image-wrapper">
+                        <div class="single-products">
+                            <div class="productinfo text-center">
+                                <?= Html::img("/images/products/{$hit->img}", ['alt' => $hit->name]) ?>
+                                <h2>$<?= $hit->price; ?></h2>
+                                <p><?= yii\helpers\Html::a("$hit->name", ['/product/view', 'id' => $hit->id]); ?></p>
+                                <a href="#" data-id="1" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                            </div>
                         </div>
                     </div>
                 </div>
+            <?php endforeach; ?>
             </div>
-            <div class="col-sm-4">
-                <div class="product-image-wrapper">
-                    <div class="single-products">
-                        <div class="productinfo text-center">
-                            <img src="/images/products/product2.jpg" alt="Джинсы">
-                            <h2>$10</h2>
-                            <p><a href="/product/1">Джинсы</a></p>
-                            <a href="#" data-id="1" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php endif; ?>
     </div>
 </div>
 
